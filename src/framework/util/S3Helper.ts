984761646f5b6016337febcb31c999a2c5cf24bb
jcjimenezvar <jjimenezv24@gmail.com>
2021-09-29 23:24:39 -0500

    Change S3Helper Function

--- src/framework/util/S3Helper.ts
+++ src/framework/util/S3Helper.ts
@@ -5,29 +5,16 @@ import { IS3Helper } from "@src/core/interfaces/IS3Helper";
 
 export class S3Helper implements IS3Helper {
   constructor() {}
-  get(bucket: string, key: string): Promise<string> {
-    return new Promise(function (resolve, reject) {
-      try {
-        const s3 = new AWS.S3({
-          accessKeyId: "SRPAGOTEST",
-          secretAccessKey: "SRPAGOTEST",
-          region: "us-east-1",
-        });
-        s3.getObject({ Key: key, Bucket: bucket })
-          .promise()
-          .then((res) => {
-            console.log("res=>", res.Body);
-            if (res.Body) resolve(res.Body?.toString());
-            // if (res.Body) {
-            //   fs.writeFile("test.txt", res.Body.toString(), (err) => {
-            //     if (err) console.log("Error, file bucket", err);
-            //   });
-            // }
-          })
-          .catch((err) => reject(JSON.stringify(err)));
-      } catch (err) {
-        reject("12");
-      }
+  public get = async (bucket: string, key: string): Promise<string> => {
+    AWS.config.update({
+      region: process.env.AWS_REGION,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     });
-  }
+    
+      const s3 = new AWS.S3();
+      const data = await s3.getObject({ Key: key, Bucket: bucket }).promise();
+      if (!data || !data.Body) throw new Error('An error occurred trying to get file from Bucket')
+      return data.Body.toString();
+  };
 }
